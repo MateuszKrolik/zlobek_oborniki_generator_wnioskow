62d82f5ca162622be425c32a39264b81dba5702a
mateuszkrolik <mateuszkrolik@Mateuszs-MacBook-Air.local>
2025-07-12 19:50:11 +0200

    embedding file system

--- services/formGenerator.go
+++ services/formGenerator.go
@@ -2,11 +2,15 @@ package services
 
 import (
 	"bytes"
+	"embed"
 	"fmt"
 	"text/template"
 
 	"github.com/MateuszKrolik/zlobek_oborniki_generator_wnioskow/models"
 )
+
+//go:embed templates/*.html
+var templateFS embed.FS
 
 type FormGenerator struct{}
 
@@ -25,7 +29,14 @@ func (FormGenerator) GeneratePages(formData models.FormData) (map[int][]byte, er
 }
 
 func generatePage(pageNumber int, formData models.FormData) ([]byte, error) {
-	tmpl, err := template.ParseFiles(fmt.Sprintf("templates/page%d.html", pageNumber))
+	// Read template from embedded filesystem
+	tmplFile, err := templateFS.ReadFile(fmt.Sprintf("templates/page%d.html", pageNumber))
+	if err != nil {
+		return nil, fmt.Errorf("error reading template: %w", err)
+	}
+
+	// Parse the template
+	tmpl, err := template.New(fmt.Sprintf("page%d", pageNumber)).Parse(string(tmplFile))
 	if err != nil {
 		return nil, fmt.Errorf("error parsing template: %w", err)
 	}
